1a95987a4130a9156da73b8c86430e3acdae4eb0
Andy Duverneau <77910275+andydu001@users.noreply.github.com>
2025-03-06 19:17:27 -0500

    Add files via upload

---
+++ NV9/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+     new  NV9Base(args);
+
+    }
+
+}
